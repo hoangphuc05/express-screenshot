cd5665ea359f09487390921264b72798375d519b
Phuc Hoang <chphuc05@gmail.com>
2022-02-27 00:35:06 -0800

    add cors and captcha

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
 const express = require('express')
 const fs = require('fs');
+var cors = require('cors')
+require('dotenv').config()
+const axios = require('axios');
+
 const app = express()
 const port = 3000
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const res = require('express/lib/response');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cors());
+// app.options('*', cors()) // include before other routes
 
+function verifyGoogleRecaptcha(captchaToken) {
+    return new Promise((resolve, reject) => {
+        const url = 'https://www.google.com/recaptcha/api/siteverify';
+        const data = {
+            secret: process.env.RECAPTCHA_SECRET,
+            response: captchaToken
+        };
+        axios({
+            method: 'POST',
+            url: url,
+            data: data
+        }).then(res => res.data)
+        .then(res => {
+            console.log(res);
+            if (res.success) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
 app.get('/', (req, res) => {
     res.send('Hello, welcome to ACM CTF API services. Please don\'t hack this server directly.');
 });
 
 app.post('/screenshot', async (req, res) => {
     console.log(req.body.html);
-    let htmlRequest = req.body.html?? "";
+    let htmlRequest = req.body.html|| "";
+    let gCaptcha = req.body['gcaptcha']||"";
     if (htmlRequest.length <= 0) {
         res.send("No html content");
         return;
     }
+
+    let captchResult = await verifyGoogleRecaptcha(gCaptcha);
+
+    if (!captchResult) {
+        res.send("Invalid captcha");
+        return;
+    }
     fs.writeFileSync(`public/${require('md5')(htmlRequest)}.html`, htmlRequest, 'utf8');
-    // res.send('Hello World!');
+    
     let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().addArguments('--headless')).build();
     try {
         
